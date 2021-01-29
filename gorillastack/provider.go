0225ed5ee12704c6df8079f52329e871435d8d56
Aashwin Varshney <aashwin.varshney@gmail.com>
2021-01-29 14:24:43 +1100

    Add testing github actions workflow.

--- gorillastack/provider.go
+++ gorillastack/provider.go
@@ -1,9 +1,6 @@
 package gorillastack
 
 import (
-	// "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	// "github.com/hashicorp/terraform-plugin-sdk/terraform"
-	// "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
